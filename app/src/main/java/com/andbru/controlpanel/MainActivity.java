3fe9b69296b48fe742e83b26ab0d46d96f6c7136
Anders Bruse <anders.bruse@gmail.com>
2016-11-10 21:12:02 +0100

    TcpClient added and its called regularly from mStatusChecker in UI thread

--- app/src/main/java/com/andbru/controlpanel/MainActivity.java
+++ app/src/main/java/com/andbru/controlpanel/MainActivity.java
@@ -1,6 +1,7 @@
 package com.andbru.controlpanel;
 
-import android.support.annotation.Nullable;
+import android.os.Handler;
+//import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,25 +19,30 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.os.AsyncTask;
+
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
+
     private PilotFragment mPilotFragment;
+    static public TextView pilotLabel;
+    private CompassFragment mCompassFragment;
+    private SettingsFragment mSettingsFragment;
+
+    private TCPClient mTcpClient;
+
+    private int mInterval = 5000;
+    private Handler mHandler;
+
+    //we create a TCPClient object and
+    //mTcpClient = new TCPClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
+
+        // Create the adapter
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
@@ -57,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
 
         mPilotFragment = new PilotFragment();
+        mCompassFragment = new CompassFragment();
+        mSettingsFragment = new SettingsFragment();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -64,11 +72,24 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                mTcpClient.sendMessage("$GET ");
             }
         });
 
+        // connect to the server
+        new connectTask().execute("");
+
+        mHandler = new Handler();
+        startRepeatingTask();
+
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRepeatingTask();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -92,53 +113,79 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
 
     public static class PilotFragment extends Fragment {
+
+        public PilotFragment() {
+        }
+
         @Override
-        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_pilot, container, false);
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View mPilotView = inflater.inflate(R.layout.fragment_pilot, container, false);
+            pilotLabel = (TextView) mPilotView.findViewById(R.id.pilot_label);
+            pilotLabel.setText("Pilot 2");
+            return mPilotView;
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+        }
+
+        public void setTextLabel(String mText) {
+            if(pilotLabel != null)pilotLabel.setText(mText);
         }
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
+
+    public static class CompassFragment extends Fragment {
+
+        public CompassFragment() {
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_compass, container, false);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+        }
+    }
+
+
+    public static class SettingsFragment extends Fragment {
+
+        public SettingsFragment() {
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_settings, container, false);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+        }
+    }
+
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -147,13 +194,18 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            if(position == 0) {
-                return mPilotFragment;
-            } else {
-                return PlaceholderFragment.newInstance(position + 1);
+
+            switch (position) {
+                case 0:
+                    return mPilotFragment;
+                case 1:
+                    return mCompassFragment;
+                case 2:
+                    return mSettingsFragment;
+                default:
+                    return mPilotFragment;
             }
+
         }
 
         @Override
@@ -168,11 +220,77 @@ public class MainActivity extends AppCompatActivity {
                 case 0:
                     return "Pilot";
                 case 1:
-                    return "SECTION 2";
+                    return "Compass";
                 case 2:
-                    return "SECTION 3";
+                    return "Settings";
+                default:
+                    return null;
             }
-            return null;
+
         }
     }
+
+    public class connectTask extends AsyncTask<String,String,TCPClient> {
+
+        @Override
+        protected TCPClient doInBackground(String... message) {
+
+            //we create a TCPClient object and
+            mTcpClient = new TCPClient(new TCPClient.OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+            });
+            mTcpClient.run();
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+            //in the arrayList we add the messaged received from server
+            //arrayList.add(values[0]);
+            // notify the adapter that the data set has changed. This means that new message received
+            // from server was added to the list
+            //mAdapter.notifyDataSetChanged();
+
+            int pnr = mViewPager.getCurrentItem();
+            if(pnr == 0) {
+                mPilotFragment.setTextLabel(values[0]);
+            }
+        }
+    }
+
+
+    Runnable mStatusChecker = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //updateStatus(); //this function can change value of mInterval.
+                    int pnr = mViewPager.getCurrentItem();
+
+                    //mPilotFragment.setTextLabel("Fantastiskt");
+                    if(mTcpClient != null) mTcpClient.sendMessage("$GET ");
+
+                } finally {
+                    // 100% guarantee that this always happens, even if
+                    // your update method throws an exception
+                    mHandler.postDelayed(mStatusChecker, mInterval);
+                }
+            }
+        };
+
+        void startRepeatingTask() {
+            mStatusChecker.run();
+        }
+
+        void stopRepeatingTask() {
+            mHandler.removeCallbacks(mStatusChecker);
+        }
 }
+
