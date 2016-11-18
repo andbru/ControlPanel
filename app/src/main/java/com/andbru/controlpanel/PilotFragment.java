6caa5982caf74b67bd194d41455d350efa4f4808
Anders Bruse <anders.bruse@gmail.com>
2016-11-18 17:29:35 +0100

    PilotFragment fixed

--- app/src/main/java/com/andbru/controlpanel/PilotFragment.java
+++ app/src/main/java/com/andbru/controlpanel/PilotFragment.java
@@ -1,17 +1,30 @@
 package com.andbru.controlpanel;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
  * Created by Anders on 2016-11-11.
  */
 
-public class PilotFragment extends Fragment {
+public class PilotFragment extends Fragment implements View.OnClickListener{
+
+    TextView rudder;
+    TextView speed;
+    Button biStdby;
+    Button biHeadingHold;
+    Button biGotoWpt;
+
+    PassCmd cmdPasser;
+
+    Compass mCompass = null;
 
     static public TextView pilotLabel;
 
@@ -26,7 +39,32 @@ public class PilotFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View mPilotView = inflater.inflate(R.layout.fragment_pilot, container, false);
-        pilotLabel = (TextView) mPilotView.findViewById(R.id.pilot_label);
+
+        mCompass = new Compass(container.getContext());
+        mCompass.setBackgroundColor(0xffffffff);
+        RelativeLayout insertCompass = (RelativeLayout)  mPilotView.findViewById(R.id.csurface);
+        insertCompass.addView(mCompass);
+
+        rudder = (TextView) mPilotView.findViewById(R.id.textRudder);
+        speed = (TextView) mPilotView.findViewById(R.id.textSpeed);
+        biStdby = (Button) mPilotView.findViewById(R.id.biStdby);
+        biHeadingHold = (Button) mPilotView.findViewById(R.id.biHeadingHold);
+        biGotoWpt = (Button) mPilotView.findViewById(R.id.biGotoWpt);
+
+        Button bHH = (Button)mPilotView.findViewById(R.id.bHeadingHold);
+        bHH.setOnClickListener(this);
+        Button bStdby = (Button)mPilotView.findViewById(R.id.bStdby);
+        bStdby.setOnClickListener(this);
+        Button bMinus5 = (Button)mPilotView.findViewById(R.id.bMinus5);
+        bMinus5.setOnClickListener(this);
+        Button bMinus1 = (Button)mPilotView.findViewById(R.id.bMinus1);
+        bMinus1.setOnClickListener(this);
+        Button bPlus1 = (Button)mPilotView.findViewById(R.id.bPlus1);
+        bPlus1.setOnClickListener(this);
+        Button bPlus5 = (Button)mPilotView.findViewById(R.id.bPlus5);
+        bPlus5.setOnClickListener(this);
+
+        pilotLabel = (TextView) mPilotView.findViewById(R.id.t1);
         //pilotLabel.setText("Pilot 2");
         pilotLabel.setText("xxx");
         return mPilotView;
@@ -37,11 +75,87 @@ public class PilotFragment extends Fragment {
         super.onPause();
     }
 
-    public void setTextLabel(String mText) {
-        if(pilotLabel != null)pilotLabel.setText(mText);
+    @Override
+    public void onAttach(Context c) {
+        super.onAttach(c);
+        cmdPasser = (PassCmd) c;
+    }
+
+    public interface PassCmd {
+        public void passCmd(String cmd);
     }
 
     public void update(PilotData mPD) {
-        if(pilotLabel != null)pilotLabel.setText(mPD.yawIs);
+
+        // Update the compass
+        double dYawCmd = new Double(mPD.yawCmd);
+        double dYawIs = new Double(mPD.yawIs);
+        mCompass.setCourses(dYawCmd, dYawIs);
+        mCompass.invalidate();
+
+        // Update measured values and status indications
+        if(rudder != null) rudder.setText(mPD.rudderPID);
+        if(speed != null) speed.setText(mPD.gpsSpeed);
+
+        int s = 0x00000000;
+        int h = 0x00000000;
+        int w = 0x00000000;
+
+        switch (mPD.mode) {
+            case "1":
+                s = 0xFF00FF00;
+                h = 0xFFFFFF00;
+                w = 0xFFFF0000;
+                break;
+            case "2":
+                s = 0xFFFFFF00;
+                h = 0xFF00FF00;
+                w = 0xFFFF0000;
+                break;
+            case "3":
+                s = 0xFFFF0000;
+                h = 0xFFFF0000;
+                w = 0xFFFF0000;
+                break;
+            default:
+                s = 0xFFFFFF00;
+                h = 0xFFFFFF00;
+                w = 0xFF00FF00;
+        }
+
+        try {
+            biStdby.setBackgroundColor(s);
+            biHeadingHold.setBackgroundColor(h);
+            biGotoWpt.setBackgroundColor(w);
+        } catch ( NullPointerException e) {
+            // Ignore, the fragment has been replaced by another before call has been invoked.
+        }
+
+        if(pilotLabel != null)pilotLabel.setText(mPD.yawCmd);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.bHeadingHold:
+                cmdPasser.passCmd("bHH");
+                break;
+            case R.id.bStdby:
+                cmdPasser.passCmd("bStdby");
+                break;
+            case R.id.bMinus5:
+                cmdPasser.passCmd("bMinus5");
+                break;
+            case R.id.bMinus1:
+                cmdPasser.passCmd("bMinus1");
+                break;
+            case R.id.bPlus1:
+                cmdPasser.passCmd("bPlus1");
+                break;
+            case R.id.bPlus5:
+                cmdPasser.passCmd("bPlus5");
+                break;
+        }
     }
 }
+
